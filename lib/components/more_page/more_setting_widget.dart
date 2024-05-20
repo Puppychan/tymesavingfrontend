67b7a13c1021ac950a3363921524b6c96637222f
Nhung <nhungmaitran1412@gmail.com>
2024-05-20 22:07:21 +0700

    feat: integrate logout

--- lib/components/more_page/more_setting_widget.dart
+++ lib/components/more_page/more_setting_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tymesavingfrontend/common/app_color.dart';
 import 'package:tymesavingfrontend/common/app_text_style.dart';
+import 'package:tymesavingfrontend/services/auth_service.dart';
 
 class MoreMenuSetting extends StatefulWidget {
   const MoreMenuSetting({super.key});
@@ -10,52 +12,61 @@ class MoreMenuSetting extends StatefulWidget {
 }
 
 class _MoreMenuSettingState extends State<MoreMenuSetting> {
-
   /*
   PlaceHolder function, when merge replace it with function to redirect to 
   other screen (Settings, Wallet?, Help Center, Feedback, Contact us, About)
   */
-  void placeHolderFunction(){
+  Future<void> placeHolderFunction() async {
     //Debug here
-   debugPrint('function tapped!');
+    debugPrint('function tapped!');
   }
-  void logoutFunction(){
+
+  Future<void> logoutFunction() async {
     //Debug here
-   debugPrint('logout tapped!');
+    debugPrint('logout tapped!');
+    final authService = Provider.of<AuthService>(context, listen: false);
+    await authService.signOut();
   }
 
   @override
   Widget build(BuildContext context) {
     return Card.outlined(
-        color: AppColors.cream,
-        margin: const EdgeInsets.all(0),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 15),
-          child: Column(
-            children: [
-              /*
+      color: AppColors.cream,
+      margin: const EdgeInsets.all(0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+        child: Column(
+          children: [
+            /*
                 Containing place holder function for development! Do change it in merge !
               */
-              RowSettingTemplate('My Wallet', 'Access your wallet', const Icon(Icons.wallet),placeHolderFunction),
-              RowSettingTemplate('Setting', 'Change setting and preference', const Icon(Icons.settings),placeHolderFunction),
-              RowSettingTemplate('Help Center', 'Change setting', const Icon(Icons.help),placeHolderFunction),
-              RowSettingTemplate('Contact us', 'Send a email or direct hotline', const Icon(Icons.phone),placeHolderFunction),
-              RowSettingTemplate('About', 'Change setting', const Icon(Icons.info),placeHolderFunction),
-              RowSettingTemplate('Logout', 'Logout your account here', const Icon(Icons.logout),placeHolderFunction),
-            ],
-          ),
+            RowSettingTemplate('My Wallet', 'Access your wallet',
+                const Icon(Icons.wallet), placeHolderFunction),
+            RowSettingTemplate('Setting', 'Change setting and preference',
+                const Icon(Icons.settings), placeHolderFunction),
+            RowSettingTemplate('Help Center', 'Change setting',
+                const Icon(Icons.help), placeHolderFunction),
+            RowSettingTemplate('Contact us', 'Send a email or direct hotline',
+                const Icon(Icons.phone), placeHolderFunction),
+            RowSettingTemplate('About', 'Change setting',
+                const Icon(Icons.info), placeHolderFunction),
+            RowSettingTemplate('Logout', 'Logout your account here',
+                const Icon(Icons.logout), logoutFunction),
+          ],
         ),
+      ),
     );
   }
 }
 
 class RowSettingTemplate extends StatefulWidget {
-  const RowSettingTemplate(this.label, this.motto, this.icon, this.function, {super.key});
+  const RowSettingTemplate(this.label, this.motto, this.icon, this.function,
+      {super.key});
 
   final String label;
   final String motto;
   final Icon icon;
-  final VoidCallback function;
+  final Future<void> Function() function;
 
   @override
   State<RowSettingTemplate> createState() => _RowSettingTemplateState();
@@ -65,29 +76,33 @@ class _RowSettingTemplateState extends State<RowSettingTemplate> {
   @override
   Widget build(BuildContext context) {
     return Card.filled(
-    color: AppColors.cream,
-    child: InkWell(
-          splashColor: Colors.blue.withAlpha(50),
-          onTap: () {
-            widget.function;
-          },
-    child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Row(
-      children: [
-        Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(widget.label, style: AppTextStyles.headingMedium,),
-              Text(widget.motto, style: AppTextStyles.subHeadingSmall,),
-           ],
-          ),
-          const Expanded(child: SizedBox()),
-          Icon(widget.icon.icon),
-        ],
-       )
-      )
-    )
-    );
+        color: AppColors.cream,
+        child: InkWell(
+            splashColor: Colors.blue.withAlpha(50),
+            onTap: () async {
+              await widget.function();
+            },
+            child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.label,
+                          style: AppTextStyles.headingMedium,
+                        ),
+                        Text(
+                          widget.motto,
+                          style: AppTextStyles.subHeadingSmall,
+                        ),
+                      ],
+                    ),
+                    const Expanded(child: SizedBox()),
+                    Icon(widget.icon.icon),
+                  ],
+                ))));
   }
 }
