9792940d528fffe526539b5be00fb0d4d1fa8e11
Nhung <nhungmaitran1412@gmail.com>
2024-06-15 01:08:35 +0700

    feat: update change in backend - update user

--- lib/services/utils/get_backend_endpoint.dart
+++ lib/services/utils/get_backend_endpoint.dart
@@ -23,7 +23,7 @@ class BackendEndpoints {
   static String get signup => "$baseUrl/user/signup";
   static String get transaction => "$baseUrl/transaction";
   // if there is no $baseUrl, the URL will be concatenated with other endpoints
-  static String get userUpdate => "update/profile";
+  static String get userUpdate => "update";
   static String get userPasswordUpdate => "update/password";
   static String get transactionReport => "report";
 }
