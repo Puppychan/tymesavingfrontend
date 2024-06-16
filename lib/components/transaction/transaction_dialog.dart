34165522f8fbf885078b7d0b32156b2614ab3b7a
vothanhthong <tthong.vox@gmail.com>
2024-06-16 10:29:03 +0700

    Create UI for Popup of each transaction

--- lib/components/transaction/transaction_dialog.dart
+++ lib/components/transaction/transaction_dialog.dart
@@ -1,48 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:tymesavingfrontend/models/transaction.model.dart';
+import 'package:tymesavingfrontend/components/transaction/infor_row.dart';
 
 class TransactionDialog extends StatelessWidget {
   final Transaction transaction;
   final String formattedDate;
 
-  TransactionDialog({required this.transaction, required this.formattedDate});
+  const TransactionDialog({
+    super.key,
+    required this.transaction,
+    required this.formattedDate,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text(transaction.category),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Type: ${transaction.type}'),
-          Text('Category: ${transaction.category}'),
-          Text('Date: $formattedDate'),
-          Text('Amount: \$${transaction.amount.toStringAsFixed(2)}'),
-        ],
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-            // Add your edit functionality here
-          },
-          child: Text('Edit'),
-        ),
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-            // Add your delete functionality here
-          },
-          child: Text('Delete'),
-        ),
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('Close'),
-        ),
-      ],
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          title: Text(transaction.category),
+          content: Container(
+            width: constraints.maxWidth * 0.8, // 80% of screen width
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InfoRow(
+                  icon: Icons.label,
+                  iconColor: Colors.blue,
+                  label: 'Type:',
+                  value: transaction.type,
+                ),
+                const SizedBox(height: 8),
+                InfoRow(
+                  icon: Icons.category,
+                  iconColor: Colors.green,
+                  label: 'Category:',
+                  value: transaction.category,
+                ),
+                const SizedBox(height: 8),
+                InfoRow(
+                  icon: Icons.date_range,
+                  iconColor: Colors.orange,
+                  label: 'Date:',
+                  value: formattedDate,
+                ),
+                const SizedBox(height: 8),
+                InfoRow(
+                  icon: Icons.attach_money,
+                  iconColor: Colors.red,
+                  label: 'Amount:',
+                  value: '\$${transaction.amount.toStringAsFixed(2)}',
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Add your edit functionality here
+              },
+              child: const Text('Edit'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Add your delete functionality here
+              },
+              child: const Text('Delete'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
