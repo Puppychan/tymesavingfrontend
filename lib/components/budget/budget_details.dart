41243e3f4a9af913c64d48d6f77d559f0a83d05c
Nhung <nhungmaitran1412@gmail.com>
2024-07-09 14:13:49 +0700

    merge: merge transaction list to budget page

--- lib/components/budget/budget_details.dart
+++ lib/components/budget/budget_details.dart
@@ -1,22 +1,20 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:tymesavingfrontend/components/common/chart/budget_pie_chart.dart';
 import 'package:tymesavingfrontend/components/common/heading.dart';
 import 'package:tymesavingfrontend/components/common_group/group_heading_actions.dart';
+import 'package:tymesavingfrontend/components/transaction/transaction_list.dart';
 import 'package:tymesavingfrontend/main.dart';
 import 'package:tymesavingfrontend/models/budget_model.dart';
 import 'package:tymesavingfrontend/models/summary_user_model.dart';
+import 'package:tymesavingfrontend/models/transaction_model.dart';
 import 'package:tymesavingfrontend/services/auth_service.dart';
 import 'package:tymesavingfrontend/services/budget_service.dart';
 import 'package:tymesavingfrontend/services/multi_page_form_service.dart';
 import 'package:tymesavingfrontend/services/user_service.dart';
 import 'package:tymesavingfrontend/utils/format_amount.dart';
 import 'package:tymesavingfrontend/utils/handling_error.dart';
-import 'package:tymesavingfrontend/components/transaction/transaction_list.dart';
-import 'package:tymesavingfrontend/models/transaction_model.dart';
-import 'package:tymesavingfrontend/services/transaction_service.dart';
 
 class BudgetDetails extends StatefulWidget {
   const BudgetDetails({super.key, required this.budgetId});
@@ -40,6 +38,19 @@ class _BudgetDetailsState extends State<BudgetDetails> with RouteAware {
   bool _isDisplayRestDescription = false;
   List<Transaction> _transactions = [];
 
+  Future<void> _loadTransactions() async {
+    if (!mounted) return;
+    final budgetService = Provider.of<BudgetService>(context, listen: false);
+    await handleMainPageApi(context, () async {
+      return await budgetService.fetchTransactionsForBudget(widget.budgetId);
+    }, () async {
+      if (!mounted) return;
+      setState(() {
+        _transactions = budgetService.transactions;
+      });
+    });
+  }
+
   Future<void> _renderUser(String? userId) async {
     Future.microtask(() async {
       if (!mounted) return;
@@ -51,19 +62,6 @@ class _BudgetDetailsState extends State<BudgetDetails> with RouteAware {
         setState(() {
           _user = userService.summaryUser;
         });
-      });
-    });
-  }
-
-  Future<void> _loadTransactions() async {
-    if (!mounted) return;
-    final budgetService = Provider.of<BudgetService>(context, listen: false);
-    await handleMainPageApi(context, () async {
-      return await budgetService.fetchTransactionsForBudget(widget.budgetId);
-    }, () async {
-      if (!mounted) return;
-      setState(() {
-        _transactions = budgetService.transactions;
       });
     });
   }
@@ -151,128 +149,71 @@ class _BudgetDetailsState extends State<BudgetDetails> with RouteAware {
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
-      appBar: Heading(title: 'Budget', showBackButton: true, actions: [
-        IconButton(
-          icon: const Icon(FontAwesomeIcons.ellipsis),
-          onPressed: () {
-            showGroupActionBottonSheet(
-                context, isMember, true, widget.budgetId);
-          },
-        )
-      ]),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                Center(
-                  child: Card.filled(
-                    color: colorScheme.onPrimary,
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            _budget!.name,
-                            style: Theme.of(context).textTheme.titleLarge,
-                            textAlign: TextAlign.center,
-                            maxLines: 2,
-                          ),
-                          const SizedBox(
-                            height: 6,
-                          ),
-                          Text.rich(
-                            TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: 'Hosted by ',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .titleSmall!
-                                      .copyWith(
-                                          color: colorScheme.secondary,
-                                          fontStyle: FontStyle.italic),
-                                ),
-                                TextSpan(
-                                  text: _user?.fullname ?? 'Loading..',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .titleSmall!
-                                      .copyWith(
-                                          color: colorScheme.secondary,
-                                          fontStyle: FontStyle.italic),
-                                ),
-                              ],
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Divider(),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          InkWell(
-                            onTap: () {
-                              // Action to view the rest of the description. This could open a dialog, a new page, or expand the text in place.
-                              setState(() {
-                                _isDisplayRestDescription =
-                                    !_isDisplayRestDescription;
-                              });
-                            },
-                            child: Text(
-                              _budget!.description,
-                              style: Theme.of(context).textTheme.bodyMedium,
+        appBar: Heading(title: 'Budget', showBackButton: true, actions: [
+          IconButton(
+            icon: const Icon(FontAwesomeIcons.ellipsis),
+            onPressed: () {
+              showGroupActionBottonSheet(
+                  context, isMember, true, widget.budgetId);
+            },
+          )
+        ]),
+        body: isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Column(
+                children: [
+                  Center(
+                    child: Card.filled(
+                      color: colorScheme.onPrimary,
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              _budget!.name,
+                              style: Theme.of(context).textTheme.titleLarge,
                               textAlign: TextAlign.center,
-                              maxLines: _isDisplayRestDescription ? null : 1,
-                              overflow: _isDisplayRestDescription
-                                  ? TextOverflow.visible
-                                  : TextOverflow.fade,
+                              maxLines: 2,
                             ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Divider(),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text.rich(
-                            TextSpan(
-                              text: 'You have ',
-                              style: Theme.of(context).textTheme.bodyMedium,
-                              children: [
-                                TextSpan(
-                                  text:
-                                      '$daysLeft', // Display the daysLeft variable here
-                                  style: Theme.of(context).textTheme.bodyLarge,
-                                ),
-                                TextSpan(
-                                  text:
-                                      ' day${daysLeft != 1 ? 's' : ''} left', // Pluralize based on the value of daysLeft
-                                ),
-                              ],
+                            const SizedBox(
+                              height: 6,
                             ),
-                          ),
-                          Text.rich(
-                            TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: 'Used ',
-                                  style: Theme.of(context).textTheme.bodyMedium,
-                                ),
-                                TextSpan(
-                                  text: _displayPercentageTaken,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyLarge, // Customize this style as needed
-                                ),
-                              ],
+                            Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: 'Hosted by ',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleSmall!
+                                        .copyWith(
+                                            color: colorScheme.secondary,
+                                            fontStyle: FontStyle.italic),
+                                  ),
+                                  TextSpan(
+                                    text: _user?.fullname ?? 'Loading..',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleSmall!
+                                        .copyWith(
+                                            color: colorScheme.secondary,
+                                            fontStyle: FontStyle.italic),
+                                  ),
+                                ],
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const Divider(),
+                            const SizedBox(
+                              height: 10,
                             ),
                             InkWell(
                               onTap: () {
@@ -346,43 +287,17 @@ class _BudgetDetailsState extends State<BudgetDetails> with RouteAware {
                       ),
                     ),
                   ),
-                ),
-                BudgetPieChart(
-                    amount: percentageTaken!.isInfinite ? 0 : percentageTaken!,
-                    concurrent: percentageLeft!),
-                const SizedBox(
-                  height: 20,
-                ),
-                // TransactionList(transactions: _transactions),
-                // Card.filled(
-                //     margin: const EdgeInsets.symmetric(
-                //         horizontal: 5, vertical: 10),
-                //     child: Column(
-                //       children: [
-                //         Text(
-                //           'Description',
-                //           style: Theme.of(context).textTheme.headlineMedium,
-                //         ),
-                //         const SizedBox(
-                //           height: 10,
-                //         ),
-                //         Card.filled(
-                //           margin: const EdgeInsets.symmetric(horizontal: 30),
-                //           child: Text(
-                //             _budget!.description,
-                //             style: Theme.of(context).textTheme.bodyMedium,
-                //             textAlign: TextAlign.center,
-                //             overflow: TextOverflow.clip,
-                //           ),
-                //         ),
-                //       ],
-                //     )),
-                // const Expanded(child: SizedBox()),
-                Expanded(
-                  child: TransactionList(transactions: _transactions),
-                ),
-              ],
-            ),
-    );
+                  BudgetPieChart(
+                      amount:
+                          percentageTaken!.isInfinite ? 0 : percentageTaken!,
+                      concurrent: percentageLeft!),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    child: TransactionList(transactions: _transactions),
+                  ),
+                ],
+              ));
   }
 }
