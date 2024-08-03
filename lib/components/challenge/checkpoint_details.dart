a4e51f446a66a84dd552fd8b5f5b3973d0b1cd99
Giang Trong Duong <trongduong04112003@gmail.com>
2024-08-03 21:12:01 +0700

    Update checkpoint_details.dart

--- lib/components/challenge/checkpoint_details.dart
+++ lib/components/challenge/checkpoint_details.dart
@@ -4,7 +4,9 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:tymesavingfrontend/components/common/heading.dart';
 import 'package:tymesavingfrontend/models/checkpoint_model.dart';
+import 'package:tymesavingfrontend/models/summary_user_model.dart';
 import 'package:tymesavingfrontend/services/challenge_service.dart';
+import 'package:tymesavingfrontend/services/user_service.dart';
 import 'package:tymesavingfrontend/utils/format_amount.dart';
 import 'package:tymesavingfrontend/utils/handling_error.dart';
 
@@ -20,9 +22,12 @@ class CheckPointDetails extends StatefulWidget {
 
 class _CheckPointDetailsState extends State<CheckPointDetails> {
   CheckPointModel? _checkPointModel;
-  bool isLoading = true;
+  SummaryUser? _challengeOwner;
   String? createDateFormatted;
   String? endDateFormatted;
+
+  bool _isDisplayRestDescription = false;
+  bool isLoading = true;
 
   Future<void> _loadCheckPoint () async {
     Future.microtask(() async {
@@ -37,7 +42,23 @@ class _CheckPointDetailsState extends State<CheckPointDetails> {
         _checkPointModel = challengeService.checkPointModel;
         createDateFormatted = formatDate(DateTime.parse(_checkPointModel!.startDate));
         endDateFormatted = formatDate(DateTime.parse(_checkPointModel!.endDate));
-        isLoading = false;
+      });
+      await _loadUser(_checkPointModel?.createdBy);
+    });
+  }
+
+  Future<void> _loadUser(String? userId) async {
+    Future.microtask(() async {
+      if(!mounted) return;
+      final userService = Provider.of<UserService>(context, listen: false);
+      await handleMainPageApi(context, () async {
+        return await userService.getOtherUserInfo(userId);
+      }, () async {
+        if (!mounted) return;
+        setState(() {
+          _challengeOwner = userService.summaryUser;
+         isLoading = false;
+        });
       });
     });
   }
@@ -76,53 +97,88 @@ class _CheckPointDetailsState extends State<CheckPointDetails> {
           Center(
             child: isLoading ?
             const Center(child: CircularProgressIndicator()) :
-            Card(
-              margin: const EdgeInsets.all(16.0),
-              elevation: 8,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      _checkPointModel!.name,
-                      style: Theme.of(context).textTheme.headlineSmall,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(),
-                    Row(
+            Column(
+              children: [
+                Card(
+                  margin: const EdgeInsets.all(16.0),
+                  elevation: 8,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        Text(
+                          _checkPointModel!.name,
+                          style: Theme.of(context).textTheme.headlineSmall,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 10,),
+                        Row(
                           children: [
-                            Text('Checkpoint by', style: Theme.of(context).textTheme.bodyMedium),
-                            const SizedBox(height: 10,),
-                            Text('Start on', style: Theme.of(context).textTheme.bodyMedium),
-                            const SizedBox(height: 10,),
-                            Text('End on', style: Theme.of(context).textTheme.bodyMedium),
-                            const SizedBox(height: 10,),
-                            Text('Milestone amount', style: Theme.of(context).textTheme.bodyMedium),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Checkpoint by', style: Theme.of(context).textTheme.bodyMedium),
+                                const SizedBox(height: 10,),
+                                Text('Start on', style: Theme.of(context).textTheme.bodyMedium),
+                                const SizedBox(height: 10,),
+                                Text('End on', style: Theme.of(context).textTheme.bodyMedium),
+                                const SizedBox(height: 10,),
+                                Text('Milestone amount', style: Theme.of(context).textTheme.bodyMedium),
+                              ],
+                            ),
+                            const Expanded(child: SizedBox()),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(_challengeOwner!.fullname, style: Theme.of(context).textTheme.bodyMedium),
+                                const SizedBox(height: 10,),
+                                Text(createDateFormatted ?? '', style: Theme.of(context).textTheme.bodyMedium),
+                                const SizedBox(height: 10,),
+                                Text(endDateFormatted ?? '', style: Theme.of(context).textTheme.bodyMedium),
+                                const SizedBox(height: 10,),
+                                Text(formatAmountToVnd(_checkPointModel!.checkPointValue), style: Theme.of(context).textTheme.bodyMedium),
+                              ],
+                            )
                           ],
                         ),
-                        const Expanded(child: SizedBox()),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text("name", style: Theme.of(context).textTheme.bodyMedium),
-                            const SizedBox(height: 10,),
-                            Text(createDateFormatted ?? '', style: Theme.of(context).textTheme.bodyMedium),
-                            const SizedBox(height: 10,),
-                            Text(endDateFormatted ?? '', style: Theme.of(context).textTheme.bodyMedium),
-                            const SizedBox(height: 10,),
-                            Text(formatAmountToVnd(_checkPointModel!.checkPointValue), style: Theme.of(context).textTheme.bodyMedium),
-                          ],
-                        )
+                        const SizedBox(height: 20),
+                        Text("Description", style: Theme.of(context).textTheme.titleSmall),
+                        const SizedBox(height: 5),
+                        InkWell(
+                              onTap: () {
+                                setState(() {
+                                  _isDisplayRestDescription = !_isDisplayRestDescription;
+                                });
+                              },
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(horizontal: 20),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      _checkPointModel!.description,
+                                      style: Theme.of(context).textTheme.bodyMedium,
+                                      textAlign: TextAlign.center,
+                                      maxLines: _isDisplayRestDescription ? null : 2,
+                                      overflow: _isDisplayRestDescription
+                                          ? TextOverflow.visible
+                                          : TextOverflow.fade,
+                                    ),
+                                    if (!_isDisplayRestDescription)
+                                      Text(
+                                        "Tap for more",
+                                        style: Theme.of(context).textTheme.labelMedium,
+                                      ),
+                                  ],
+                                ),
+                              ),
+                            ),
                       ],
                     ),
-                    // Add more details here
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
         ],
