5523191ab2f31d406fc07dbe78a25fd1727c66f5
Giang Trong Duong <trongduong04112003@gmail.com>
2024-08-16 21:28:33 +0700

    Budget report and saving report finalization

--- lib/components/group_saving/group_saving_report.dart
+++ lib/components/group_saving/group_saving_report.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:tymesavingfrontend/components/common/chart/report_pie_chart_categories.dart';
 import 'package:tymesavingfrontend/components/common/chart/report_pie_chart_user.dart';
@@ -8,6 +9,7 @@ import 'package:tymesavingfrontend/models/report_model.dart';
 import 'package:tymesavingfrontend/models/transaction_model.dart';
 import 'package:tymesavingfrontend/services/budget_service.dart';
 import 'package:tymesavingfrontend/services/group_saving_service.dart';
+import 'package:tymesavingfrontend/utils/format_amount.dart';
 import 'package:tymesavingfrontend/utils/handling_error.dart';
 
 class GroupSavingReport extends StatefulWidget {
@@ -23,6 +25,7 @@ class _GroupSavingReportState extends State<GroupSavingReport> {
   List<ReportUser> _userList = [];
     List<ReportCategory> _categoryList = [];
     List<Transaction> _transactionList = [];
+    ReportInfo? _reportInfo;
     String filter = "latest";
     bool isLoading = true;
 
@@ -38,11 +41,17 @@ class _GroupSavingReportState extends State<GroupSavingReport> {
             _userList = savingService.reportUserList;
             _categoryList = savingService.reportCategoryList;
             _transactionList = savingService.transactions;
+            _reportInfo = savingService.reportInfo;
             isLoading = false;
           });
         });
       });
     }
+    
+  String formatDate(DateTime date){
+    String formattedDate = DateFormat('d-M, y').format(date);
+    return formattedDate;
+  }
 
   @override
   void initState() {
@@ -64,6 +73,62 @@ class _GroupSavingReportState extends State<GroupSavingReport> {
         const Center(child: CircularProgressIndicator() )
         : Column(
           children: [
+            Text(
+              "Final report of", 
+              style: textTheme.headlineMedium,
+              softWrap: true,
+              overflow: TextOverflow.visible,
+              ),
+            Text(_reportInfo!.name,style: textTheme.headlineSmall),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Text(_reportInfo!.description, 
+                        style: textTheme.bodyMedium, 
+                        overflow: TextOverflow.visible,
+                        softWrap: true,
+                        textAlign: TextAlign.justify,
+                        ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Budget at the start', style: Theme.of(context).textTheme.bodyMedium,),
+                      const Expanded(child: SizedBox()),
+                      Text(formatAmountToVnd(_reportInfo!.amount), style: Theme.of(context).textTheme.headlineMedium!.copyWith(fontSize: 15),),
+                    ],  
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Left over budget', style: Theme.of(context).textTheme.bodyMedium,),
+                      const Expanded(child: SizedBox()),
+                      Text(formatAmountToVnd(_reportInfo!.concurrentAmount), style: Theme.of(context).textTheme.headlineMedium!.copyWith(fontSize: 15),),
+                    ],
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Created on', style: Theme.of(context).textTheme.bodyMedium,),
+                      const Expanded(child: SizedBox()),
+                      Text(formatDate(_reportInfo!.createdDate), style: Theme.of(context).textTheme.headlineMedium!.copyWith(fontSize: 15),),
+                    ],
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('End on', style: Theme.of(context).textTheme.bodyMedium,),
+                      const Expanded(child: SizedBox()),
+                      Text(formatDate(_reportInfo!.endDate), style: Theme.of(context).textTheme.headlineMedium!.copyWith(fontSize: 15),),
+                    ],
+                  ),
+                ],
+              ),
+            ),
             Text("Top categories in group", style: textTheme.headlineMedium,),
             const SizedBox(height: 10,),
             ReportPieChartCategories(reportCategory: _categoryList),
