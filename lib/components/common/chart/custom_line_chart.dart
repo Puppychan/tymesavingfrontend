b83d89c1ce610003ad60c97cd2a6a8ed2029de78
Giang Trong Duong <trongduong04112003@gmail.com>
2024-08-30 19:38:33 +0700

    Update: Spending Tracking is updated and renamed to cashflow tracking

--- lib/components/common/chart/custom_line_chart.dart
+++ lib/components/common/chart/custom_line_chart.dart
@@ -1,40 +1,38 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:tymesavingfrontend/common/styles/app_extend_theme.dart';
 import 'package:tymesavingfrontend/utils/format_amount.dart';
 
 class CustomLineChart extends StatefulWidget {
   const CustomLineChart({
     super.key,
-    required this.totals,
+    required this.totalsIncome,
+    required this.totalsExpense,
   });
 
-  final Map<String, int> totals;
+  final Map<String, int> totalsIncome;
+  final Map<String, int> totalsExpense;
 
   @override
   State<CustomLineChart> createState() => _CustomLineChartState();
 }
 
 class _CustomLineChartState extends State<CustomLineChart> {
-  late List<MapEntry<String, int>> keyValuePairs;
-  late List<FlSpot> flSpots = [];
+  late List<MapEntry<String, int>> keyValuePairsIncome;
+  late List<MapEntry<String, int>> keyValuePairsExpense;
+  late List<FlSpot> incomeSpots = [];
+  late List<FlSpot> expenseSpots = [];
 
   @override
   void initState() {
-    // List to hold key-value pairs as tuples
-    keyValuePairs = [];
-
-    // Extract and insert key-value pairs into the list
-    widget.totals.forEach((key, value) {
-      keyValuePairs.add(MapEntry(key, value));
-    });
-
-    for (int i = 6; i < keyValuePairs.length; i++) {
-      var entry = keyValuePairs[i];
-      debugPrint('Month: ${entry.key}, Value: ${entry.value}');
-      flSpots.add(FlSpot(i.toDouble(), entry.value.toDouble()));
-    }
-
     super.initState();
+    keyValuePairsIncome = widget.totalsIncome.entries.toList();
+    keyValuePairsExpense = widget.totalsExpense.entries.toList();
+
+    for (int i = 6; i < keyValuePairsIncome.length; i++) {
+      incomeSpots.add(FlSpot(i.toDouble(), keyValuePairsIncome[i].value.toDouble()));
+      expenseSpots.add(FlSpot(i.toDouble(), keyValuePairsExpense[i].value.toDouble()));
+    }
   }
 
   @override
@@ -45,56 +43,80 @@ class _CustomLineChartState extends State<CustomLineChart> {
       child: SizedBox(
         height: 300,
         child: LineChart(
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.linear,
           LineChartData(
-              gridData: const FlGridData(
-                show: false,
+            gridData: FlGridData(
+              show: true,
+              drawVerticalLine: true,
+              getDrawingVerticalLine: (value) {
+                return FlLine(
+                  color: Colors.grey[300],
+                  strokeWidth: 1,
+                );
+              },
+              drawHorizontalLine: false,
+            ),
+            borderData: FlBorderData(
+              show: true,
+              border: Border.all(
+                color: Colors.grey[300]!,
+                width: 1,
               ),
-              borderData: FlBorderData(
-                show: false,
+            ),
+            titlesData: FlTitlesData(
+              show: true,
+              topTitles: const AxisTitles(
+                sideTitles: SideTitles(showTitles: false),
               ),
-              titlesData: FlTitlesData(
-                show: true,
-                topTitles: const AxisTitles(
-                  sideTitles: SideTitles(showTitles: false),
-                ),
-                leftTitles: const AxisTitles(
-                  sideTitles: SideTitles(showTitles: false),
-                ),
-                rightTitles: const AxisTitles(
-                  sideTitles: SideTitles(showTitles: false),
-                ),
-                bottomTitles: AxisTitles(
-                  sideTitles: SideTitles(
-                    showTitles: true,
-                    reservedSize: 90,
-                    interval: 1,
-                    getTitlesWidget: (value, meta) =>
-                        bottomTitleWidgets(value, meta),
-                  ),
+              leftTitles: const AxisTitles(
+                sideTitles: SideTitles(
+                showTitles: false,
                 ),
               ),
-              minY: 0,
-              lineBarsData: [
-                LineChartBarData(
-                  spots: flSpots,
-                  color: colorScheme.inversePrimary,
-                  barWidth: 2.5,
-                  isCurved: true,
-                  curveSmoothness: 0.3,
-                  preventCurveOverShooting: true,
-                  dotData: const FlDotData(show: true),
-                )
-              ],
-               lineTouchData: LineTouchData(
+              rightTitles: const AxisTitles(
+                sideTitles: SideTitles(showTitles: false),
+              ),
+              bottomTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  reservedSize: 30,
+                  interval: 1,
+                  getTitlesWidget: (value, meta) =>
+                      bottomTitleWidgets(value, meta),
+                ),
+              ),
+            ),
+            minY: 0,
+            lineBarsData: [
+              LineChartBarData(
+                spots: incomeSpots,
+                color: colorScheme.success,
+                barWidth: 2.5,
+                isCurved: true,
+                curveSmoothness: 0.3,
+                preventCurveOverShooting: true,
+                dotData: const FlDotData(show: true),
+              ),
+              LineChartBarData(
+                spots: expenseSpots,
+                color: colorScheme.error,
+                barWidth: 2.5,
+                isCurved: true,
+                curveSmoothness: 0.3,
+                preventCurveOverShooting: true,
+                dotData: const FlDotData(show: true),
+              ),
+            ],
+            lineTouchData: LineTouchData(
               touchTooltipData: LineTouchTooltipData(
                 getTooltipItems: (List<LineBarSpot> touchedSpots) {
                   return touchedSpots.map((touchedSpot) {
+                    // Use the color of the graph line for the tooltip text
+                    final textColor = touchedSpot.bar.color!;
+
                     return LineTooltipItem(
                       formatAmountToVnd(touchedSpot.y),
-                      const TextStyle(
-                        color: Colors.white,
+                      TextStyle(
+                        color: textColor, // Set the tooltip text color to the graph line color
                         fontWeight: FontWeight.bold,
                       ),
                     );
@@ -102,55 +124,51 @@ class _CustomLineChartState extends State<CustomLineChart> {
                 },
               ),
             ),
-            ), 
+          ),
         ),
       ),
     );
   }
 
   Widget bottomTitleWidgets(double value, TitleMeta meta) {
-    final style = Theme.of(context).textTheme.bodyMedium;
-
+    final style = Theme.of(context).textTheme.labelMedium!;
     Widget text;
     switch (value.toInt()) {
       case 0:
-        text = Text(keyValuePairs[0].key.toLowerCase(), style: style);
+        text = Text(keyValuePairsIncome[0].key, style: style);
         break;
-      case 1:
-        text = Text(keyValuePairs[1].key.toLowerCase(), style: style);
-        break;
+      // case 1:
+      //   text = Text(keyValuePairsIncome[1].key, style: style);
+      //   break;
       case 2:
-        text = Text(keyValuePairs[2].key.toLowerCase(), style: style);
+        text = Text(keyValuePairsIncome[2].key.toLowerCase(), style: style);
         break;
-      case 3:
-        text = Text(keyValuePairs[3].key.toLowerCase(), style: style);
-        break;
+      // case 3:
+      //   text = Text(keyValuePairsIncome[3].key, style: style);
+      //   break;
       case 4:
-        text = Text(keyValuePairs[4].key.toLowerCase(), style: style);
+        text = Text(keyValuePairsIncome[4].key, style: style);
         break;
-      case 5:
-        text = Text(keyValuePairs[5].key.toLowerCase(), style: style);
-        break;
+      // case 5:
+      //   text = Text(keyValuePairsIncome[5].key, style: style);
+      //   break;
       case 6:
-        text = Text(keyValuePairs[6].key.toLowerCase(), style: style);
+        text = Text(keyValuePairsIncome[6].key.toLowerCase(), style: style);
         break;
       case 7:
-        text = Text(keyValuePairs[7].key.toLowerCase(), style: style);
+        text = Text(keyValuePairsIncome[7].key.toLowerCase(), style: style);
         break;
       case 8:
-        text = Text(keyValuePairs[8].key.toLowerCase(), style: style);
+        text = Text(keyValuePairsIncome[8].key.toLowerCase(), style: style);
         break;
       case 9:
-        text = Text(keyValuePairs[9].key.toLowerCase(), style: style);
+        text = Text(keyValuePairsIncome[9].key.toLowerCase(), style: style);
         break;
       case 10:
-        text = Text(keyValuePairs[10].key.toLowerCase(), style: style);
+        text = Text(keyValuePairsIncome[10].key.toLowerCase(), style: style);
         break;
       case 11:
-        text = Text(keyValuePairs[11].key.toLowerCase(), style: style);
-        break;
-      case 12:
-        text = Text(keyValuePairs[12].key.toLowerCase(), style: style);
+        text = Text(keyValuePairsIncome[11].key.toLowerCase(), style: style);
         break;
       default:
         text = Text('', style: style);
@@ -159,7 +177,8 @@ class _CustomLineChartState extends State<CustomLineChart> {
 
     return SideTitleWidget(
       axisSide: meta.axisSide,
-      child: Container(padding: const EdgeInsets.only(top: 30), child: text),
+      child: Container(padding: const EdgeInsets.only(top: 8), child: text),
     );
   }
 }
+
