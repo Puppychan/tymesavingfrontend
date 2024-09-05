92af26af385f24d2ce62598acde07121327f6ed1
Giang Trong Duong <trongduong04112003@gmail.com>
2024-09-05 19:41:04 +0700

    Fix: Update the button in actionRow to be clickable from the whole width

--- lib/components/common/sheet/icon_text_row.dart
+++ lib/components/common/sheet/icon_text_row.dart
@@ -3,15 +3,25 @@ import 'package:flutter/material.dart';
 List<Widget> actionRow(
     BuildContext context, IconData icon, String label, Function() onPressed) {
   return [
-    TextButton.icon(
-      icon: Icon(icon, size: 18),
-      label: Text(" $label", style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-        fontWeight: FontWeight.w600
-      )),
-      onPressed: () {
-        Navigator.pop(context);
-        onPressed();
-      },
+    SizedBox(
+      width: MediaQuery.of(context).size.width, // Takes full width of the screen
+      child: TextButton.icon(
+        icon: Icon(icon, size: 18),
+        label: Text(
+          " $label",
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+          onPressed();
+        },
+        style: TextButton.styleFrom(
+          alignment: Alignment.centerLeft, // Align the content to the left
+          padding: EdgeInsets.symmetric(horizontal: 16.0), // Optional padding
+        ),
+      ),
     ),
     const Divider(),
     const SizedBox(height: 15),
